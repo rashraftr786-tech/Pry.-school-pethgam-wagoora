799d1cbf7170686ed069bf57bbbd0ff2c000dfaf
rashraftr786-tech <rashraftr786@gmail.com>
2025-12-29 22:40:21 +0530

    Create app.js

---
+++ js/app.js
@@ -0,0 +1,8 @@
+// Register Service Worker for Offline Support
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/sw.js')
+      .then(reg => console.log('Service Worker Registered'))
+      .catch(err => console.log('Service Worker Failed', err));
+  });
+}
