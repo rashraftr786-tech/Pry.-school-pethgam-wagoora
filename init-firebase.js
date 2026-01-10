f9327914498f010a829c242a89c5b52bb7c98468
rashraftr786-tech <rashraftr786@gmail.com>
2026-01-10 13:15:58 +0530

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,5 +1,6 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/9.22.1/firebase-firestore.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js"; // ADD THIS
 
 const firebaseConfig = {
   apiKey: "AIzaSyCzxHme_3hrztrDuD7wupGwNilDtYLfENI",
@@ -11,5 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
 
+export const db = getFirestore(app);
+export const auth = getAuth(app); // ADD THIS
