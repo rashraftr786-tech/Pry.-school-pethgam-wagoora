64816553b7c60e3cad8e56c2936031f8fa55bd66
rashraftr786-tech <rashraftr786@gmail.com>
2026-01-12 08:05:06 +0530

    Update init-firebase.js

--- init-firebase.js
+++ init-firebase.js
@@ -1,21 +1,16 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCzxHme_3hrztrDuD7wupGwNilDtYLfENI",
   authDomain: "studio-2729610945-4684b.firebaseapp.com",
   projectId: "studio-2729610945-4684b",
   storageBucket: "studio-2729610945-4684b.firebasestorage.app",
   messagingSenderId: "711147405574",
-  appId: "1:711147405574:web:7b5597955b676a3e4d1e87",
-  measurementId: "G-26C560QG0B"
+  appId: "1:711147405574:web:7b5597955b676a3e4d1e87"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export const db = getFirestore(app);
