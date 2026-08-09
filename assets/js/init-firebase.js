addfa1050dea1a1a5cadb1264907f7cd0332ac0b
rashraftr786-tech <rashraftr786@gmail.com>
2026-08-09 13:01:19 +0530

    Update init-firebase.js

--- assets/js/init-firebase.js
+++ assets/js/init-firebase.js
@@ -1,26 +1,58 @@
 // assets/js/init-firebase.js
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-app.js";
-import { getFirestore } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
-import { getStorage } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-storage.js";
-import { getAuth } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-auth.js";
+import { initializeApp } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+
+import { getFirestore } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
+
+import { getStorage } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-storage.js";
+
+import { getAuth } from
+  "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
+
 
 const firebaseConfig = {
+
   apiKey: "AIzaSyDgVFdfKpCrLkoRsMI2gS7ajlJUzVdYLZU",
-  authDomain: "pry-school-pethgam-wagoora.firebaseapp.com",
-  projectId: "pry-school-pethgam-wagoora",
-  storageBucket: "pry-school-pethgam-wagoora.firebasestorage.app",
-  messagingSenderId: "255417366612",
-  appId: "1:255417366612:web:81cec46aa75cb7d4ba95c1",
-  measurementId: "G-JE9WY9T6FJ"
+
+  authDomain:
+    "pry-school-pethgam-wagoora.firebaseapp.com",
+
+  projectId:
+    "pry-school-pethgam-wagoora",
+
+  storageBucket:
+    "pry-school-pethgam-wagoora.firebasestorage.app",
+
+  messagingSenderId:
+    "255417366612",
+
+  appId:
+    "1:255417366612:web:81cec46aa75cb7d4ba95c1",
+
+  measurementId:
+    "G-JE9WY9T6FJ"
 };
 
+
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
+
+// Firebase services
 const db = getFirestore(app);
+
 const storage = getStorage(app);
+
 const auth = getAuth(app);
 
-console.log("Firebase initialized successfully.");
 
-export { app, db, storage, auth };
+// Export services
+export {
+  app,
+  db,
+  storage,
+  auth
+};
